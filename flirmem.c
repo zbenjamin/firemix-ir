f4625a900aeb2ee556ce98614cb988ce90660ee0
Zev Benjamin <zev@mit.edu>
2015-08-19 14:30:24 -0700

    Hopefully fix memory layout

--- flirmem.c
+++ flirmem.c
@@ -26,6 +26,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <string.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -41,7 +42,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <sys/mman.h>
 #include <sys/ipc.h>
 
-#define SHMSZ    9600 //80x60x2 bytes
+#define ROWSZ 80
+#define COLSZ 60
+#define SHMSZ ROWSZ*COLSZ*2
 //#define DEBUG
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
 
@@ -59,8 +62,9 @@ static uint16_t delay;
 
 #define VOSPI_FRAME_SIZE (164)
 uint8_t lepton_frame_packet[VOSPI_FRAME_SIZE];
-uint16_t** lepton_image;
+uint16_t* lepton_image;
 
+/*
 static void save_pgm_file(void)
 {
 	int i;
@@ -76,9 +80,9 @@ static void save_pgm_file(void)
 	}
 
 	printf("Calculating min/max values for proper scaling...\n");
-	for(i=0;i<60;i++)
+	for(i=0;i<COLSZ;i++)
 	{
-		for(j=0;j<80;j++)
+		for(j=0;j<ROWSZ;j++)
 		{
 			if (lepton_image[i][j] > maxval) {
 				maxval = lepton_image[i][j];
@@ -94,7 +98,7 @@ static void save_pgm_file(void)
 	fprintf(f,"P2\n80 60\n%u\n",maxval-minval);
 	for(i=0;i<60;i++)
 	{
-		for(j=0;j<80;j++)
+		for(j=0;j<ROWSZ;j++)
 		{
 			fprintf(f,"%d ", lepton_image[i][j] - minval);
 		}
@@ -104,6 +108,7 @@ static void save_pgm_file(void)
 
 	fclose(f);
 }
+*/
 
 int transfer(int fd)
 {
@@ -111,7 +116,8 @@ int transfer(int fd)
 	int i;
 	int frame_number = -1;
 	uint8_t tx[VOSPI_FRAME_SIZE] = {0, };
-	uint16_t* row_data;
+	uint16_t* src_row_data;
+	uint16_t* dst_row_data;
 	struct spi_ioc_transfer tr = {
 		.tx_buf = (unsigned long)tx,
 		.rx_buf = (unsigned long)lepton_frame_packet,
@@ -132,13 +138,11 @@ int transfer(int fd)
 #endif
 		frame_number = lepton_frame_packet[1];
 
-		row_data = (uint16_t*) (lepton_frame_packet + 4);
-		if(frame_number < 60 )
+		src_row_data = (uint16_t*) (lepton_frame_packet + 4);
+		dst_row_data = lepton_image + frame_number * ROWSZ;
+		if(frame_number < COLSZ )
 		{
-			for(i=0;i<80;i++)
-			{
-				lepton_image[frame_number][i] = row_data[i];
-			}
+			memcpy(dst_row_data, src_row_data, ROWSZ * 2);
 		}
 	}
 	return frame_number;
@@ -218,10 +222,10 @@ int main(int argc, char *argv[])
 	printf("bits per word: %d\n", bits);
 	printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
 
-	lepton_image = (uint16_t**) open_shm();
+	lepton_image = (uint16_t*) open_shm();
 
 	int row;
-	while ((row=transfer(fd)) != 59) {
+	while ((row=transfer(fd)) != COLSZ - 1) {
 #ifdef DEBUG
 		if (row == -1) {
 			fprintf(stderr, "Incorrect row returned, retrying...\n");
